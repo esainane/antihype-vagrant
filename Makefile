63d9e07e3c6717227748def25a7ea84c171631f7
esainane <esainane+github@gmail.com>
2016-02-16 18:02:13 +1300

    Invoke scripts from Makefile
    Call sudo only when needed, and allow for alternative ask-for-privilege
    programs through specification of a SUDO environment variable.
    
    Still builds packages as root. Will drop the current checkinstall
    mechanism in favour of a proper specfile implementation soon.

--- Makefile
+++ Makefile
@@ -1,16 +1,24 @@
 
 all: deploy
 
+.PHONY: all clean deploy buildbox core
+
 bento/builds/centos-7.2.virtualbox.box:
 	@which packer || ( echo "Packer needs to be installed, aborting!\nSee: https://www.packer.io/downloads.html"; false )
 	cd bento; bin/bento build --only=virtualbox-iso centos-7.2-x86_64
 
-rpmbuild: bento/builds/centos-7.2.virtualbox.box libbrine quassel
+buildbox: bento/builds/centos-7.2.virtualbox.box
 	if vagrant status buildbox | grep -q running; then vagrant provision buildbox; else vagrant up buildbox; fi
+
+core: bento/builds/centos-7.2.virtualbox.box
+	if vagrant status core | grep -q running; then vagrant provision core; else vagrant up core; fi
+
+rpmbuild: buildbox libbrine/.git libbrine/*.c libbrine/*.h quassel/src | libbrine quassel
+	vagrant ssh buildbox -c 'bash /vagrant/build.bash; exit $$?'
 	test -d rpmbuild
 
-deploy: rpmbuild
-	vagrant up core
+deploy: rpmbuild core
+	vagrant ssh core -c 'bash /vagrant/core.bash; exit $$?'
 
 clean:
 	vagrant destroy
--- build.bash
+++ build.bash
@@ -5,24 +5,28 @@ set -ex
 # Initial setup
 #
 
-yum groupinstall -y "Development tools";
-yum install -y rpm-build rpmdevtools;
-rpmdev-setuptree;
+# Use sudo if we're not root, and always use the "SUDO" environment variable for things that need superuser permissions. This could resolve to a simple wrapper around sup, for example.
+SUDO=${SUDO:=}
+[ $(id -u) -eq 0 ] || SUDO=${SUDO:=sudo}
+
+"$SUDO" yum groupinstall -y "Development tools";
+"$SUDO" yum install -y rpm-build rpmdevtools;
+"$SUDO" rpmdev-setuptree;
 
 cd /vagrant/;
 
-which checkinstall 2>/dev/null ||
+which checkinstall 2>/dev/null && false ||
 (
   cd checkinstall;
   make;
-  su;
-  make install;
+  "$SUDO" make install;
   # XXX: Hack around prefixing
-  ln -s /usr/local/lib/installwatch.so /usr/local/lib64/installwatch.so
-  checkinstall -R -y;
+  "$SUDO" ln -sf /usr/local/lib/installwatch.so /usr/local/lib64/installwatch.so
+  which checkinstall;
+  "$SUDO" checkinstall -R -y;
 )
 
-yum install -y git;
+"$SUDO" yum install -y git;
 
 mkdir -p /vagrant/rpmbuild/RPMS/x86_64/
 
@@ -30,32 +34,31 @@ mkdir -p /vagrant/rpmbuild/RPMS/x86_64/
 # Build and package libbrine
 #
 
-yum install -y openssl-devel glib2-devel;
+"$SUDO" yum install -y openssl-devel glib2-devel;
 
 cd /vagrant/;
 
 cd libbrine;
 make;
-checkinstall -R -y --pkgname='libbrine' --pkgversion=$(git describe --long --dirty | sed 's/-/_/g') make install PREFIX=/usr;
-cp /root/rpmbuild/RPMS/x86_64/libbrine-*.rpm /vagrant/rpmbuild/RPMS/x86_64/;
-
-# Quassel depends on libbrine to Build
-
-yum install -y /vagrant/rpmbuild/RPMS/x86_64/libbrine-*.rpm;
+# --install=yes as Quassel depends on libbrine to Build
+"$SUDO" checkinstall --exclude=/selinux,/sys -R -y --pkgname='libbrine' --install=yes --pkgversion=$(git describe --long --dirty | sed 's/-/_/g') make install PREFIX=/usr;
+BRINEPKG=$("$SUDO" ls -t /root/rpmbuild/RPMS/x86_64/ | grep libbrine-.*\.rpm | head -1)
+"$SUDO" cp "/root/rpmbuild/RPMS/x86_64/$BRINEPKG" /vagrant/rpmbuild/RPMS/x86_64/;
 
 #
 # Build and package Quassel
 #
 
-yum install -y cmake qt-devel qca2-devel;
+"$SUDO" yum install -y cmake qt-devel qca2-devel;
 
 cd /vagrant/;
 
 cd quassel;
 mkdir -p build;
 cd build;
-cmake -DWANT_CORE=yes -DWANT_QTCLIENT=no -DWANT_MONO=no -DWITH_OXYGEN=no ..;
+cmake -DWANT_CORE=yes -DWANT_QTCLIENT=no -DWANT_MONO=no -DWITH_OXYGEN=no -DWITH_BRINE=yes ..;
 make;
-# FIXME: Don't build package as root
-checkinstall -R -y --pkgname='quassel' --pkgversion=$(git describe --long --dirty | sed 's/-/_/g');
-cp /root/rpmbuild/RPMS/x86_64/quassel-*.rpm /vagrant/rpmbuild/RPMS/x86_64/;
+
+"$SUDO" checkinstall -R -y --pkgname='quassel' --install=no --pkgversion=$(git describe --long --dirty | sed 's/-/_/g');
+QUASSELPKG=$("$SUDO" ls -t /root/rpmbuild/RPMS/x86_64/ | grep quassel-.*\.rpm | head -1)
+"$SUDO" cp "/root/rpmbuild/RPMS/x86_64/$QUASSELPKG" /vagrant/rpmbuild/RPMS/x86_64/;
